c3738ab4e1b253db12f33b5bca205052d2d29753
MarcSR9 <marc.sanro@gmail.com>
2021-05-11 18:50:54 +0200

    Funciones del controlador de usuarios

--- app/Http/Controllers/EmpresasController.php
+++ app/Http/Controllers/EmpresasController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class NoticiasController extends Controller
+class EmpresasController extends Controller
 {
     public function index()
     {
