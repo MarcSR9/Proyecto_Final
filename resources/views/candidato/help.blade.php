84d2ce15d46b46d88f6546a1df47c4c916e72658
Marc <68541896+marcaclvarezruiz@users.noreply.github.com>
2021-05-25 16:47:18 +0200

    marc

---
+++ resources/views/candidato/help.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle bg-info" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Preguntas frecuentes
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Preguntas</a>
+            <a class="dropdown-item" href="#">Preguntas</a>
+            <a class="dropdown-item" href="#">Preguntas</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle bg-info" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Preguntas frecuentes
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Preguntas</a>
+            <a class="dropdown-item" href="#">Preguntas</a>
+            <a class="dropdown-item" href="#">Preguntas</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle bg-info" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Preguntas frecuentes
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Preguntas</a>
+            <a class="dropdown-item" href="#">Preguntas</a>
+            <a class="dropdown-item" href="#">Preguntas</a>
+        </div>
+    </div>
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle bg-info" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Preguntas frecuentes
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Preguntas</a>
+            <a class="dropdown-item" href="#">Preguntas</a>
+            <a class="dropdown-item" href="#">Preguntas</a>
+        </div>
+    </div>
+</div>
+@endsection
